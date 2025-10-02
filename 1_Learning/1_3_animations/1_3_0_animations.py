fd3b0da176c74615e49005b64bd37b1ee7272525
Kieran Gaydhani <kierangaydhani@kubrickgroup.com>
2025-10-02 15:51:40 +0100

    starting animation files

---
+++ 1_Learning/1_3_animations/1_3_0_animations.py
@@ -0,0 +1,88 @@
+"""
+Trying to figure out some animations, I want to combine the wall stuff with animation
+I think doing some jumping over the walls with gravity would be useful
+"""
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+dt = 0
+vy = 0.0 # +y is downwards in Pygame, so vy > 0 is moving downwards (vy being y velocity)
+GRAVITY = 1600
+JUMP_SPEED = 800
+grounded = False #bool
+SPEED = 300
+R = 40
+w, h = screen.get_size()
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_rect = pygame.Rect(0, 0, R*2, R*2)
+player_rect.center = player_pos
+
+left_border_rect = pygame.Rect(-1,0,1,h) 
+right_border_rect = pygame.Rect(w,0,1,h) 
+top_border_rect = pygame.Rect(0,-1,w,1) 
+bot_border_rect = pygame.Rect(0,h,w,1)
+
+wall_list = [left_border_rect, right_border_rect, 
+             top_border_rect, bot_border_rect]
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    keys = pygame.key.get_pressed()
+    
+    dx = (keys[pygame.K_d] - keys[pygame.K_a]) * SPEED * dt 
+
+    player_pos.x += dx 
+    player_rect.centerx = round(player_pos.x) 
+    
+    for rect in wall_list: 
+        if player_rect.colliderect(rect):
+            if dx > 0:
+                player_rect.right = rect.left
+            elif dx < 0:
+                player_rect.left = rect.right
+            
+            player_pos.x = player_rect.centerx
+    
+    vy += GRAVITY * dt #gravity is acceleration (d^2x/dt^2), so multiplying by dt, is basically integration here (in basic a roundabout fashion) to velocity
+    player_pos.y += vy * dt #velocity multipled by time is distance, hence it moves each frame by the velocity multiplied by that frame time
+    player_rect.centery = round(player_pos.y) # ensures collision tests use the rect
+    grounded = False
+    for rect in wall_list:
+        if player_rect.colliderect(rect):
+            if vy > 0: # this is case 1 where we fall onto something, i.e. there is a collision and vy is downwards (+ve), thus the moment there is collision:
+                player_rect.bottom = rect.top # we snap the bottom of the player rect to the top of wall rekt (or floor etc.)
+                player_pos.y = player_rect.centery
+                vy = 0 #we need to then instantly kill any more downwards velocity (i.e. this is acting like our normal force)
+                grounded = True #this ensures we are grounded
+                break #we only need to bother checking the first frame
+            elif vy < 0:
+                player_rect.top = rect.bottom #this is basically the same thing but for head hitting ceiling
+                player_pos.y = player_rect.centery
+                vy = 0 # in this case we dont want it to be grounded, as we want the next frame to start falling
+            
+    
+    if keys[pygame.K_SPACE] and grounded:
+        vy = -JUMP_SPEED #instant upwards velocity (this is not an accel its just for jumping)
+        grounded = False # stops us from being able to jump in the air
+
+    player_rect.center = player_pos      
+    player_pos.update(player_rect.center) 
+
+    screen.fill("white") 
+    pygame.draw.circle(screen, "black", player_pos, R)   
+    for wrect in wall_list:
+        pygame.draw.rect(screen, "green", wrect)
+    
+    pygame.display.flip() 
+    
+    dt = clock.tick(60) / 1000
+
+pygame.quit()     
